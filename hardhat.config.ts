8de7c1de4f4f5e5b48c2281b5590586ad1b983f6
0xNSHuman <0xnshuman@protonmail.com>
2022-03-11 11:12:06 -0700

    Add a bunch of hardhat plugins for better testing, analytics and documentation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,10 @@ import "@nomiclabs/hardhat-waffle";
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-solhint';
+import 'solidity-coverage';
+import 'hardhat-gas-reporter';
+import '@primitivefi/hardhat-dodoc';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
