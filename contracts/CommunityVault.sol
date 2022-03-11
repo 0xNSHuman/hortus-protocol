be6f998039d3ef8dbaf485ae391dbb7f4b20d0b9
0xNSHuman <0xnshuman@protonmail.com>
2022-03-10 23:43:39 -0700

    Implement subscription partially to accept ETH or ERC-20 as a payment

--- contracts/CommunityVault.sol
+++ contracts/CommunityVault.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./interfaces/ICommunity.sol";
 import "./interfaces/ICreatorAccess.sol";
 import "./interfaces/IMembership.sol";
@@ -14,14 +16,17 @@ contract CommunityVault is ERC721,
 Ownable,
 ICommunity,
 ICreatorAccess,
-IMembership {
+IMembership,
+ReentrancyGuard {
     using SafeMath for uint;
     using Counters for Counters.Counter;
+    using SafeERC20 for IERC20;
 
     address public commerceToken;
     uint public membershipPrice;
     Counters.Counter private _tokenIdCounter;
     mapping(address => uint) private _membershipExpirations;
+    mapping(address => uint) private _creatorFunds;
 
     constructor(
         string memory _symbol,
@@ -54,8 +59,23 @@ IMembership {
     /** 
     @notice Purchase membership
     */
-    function subscribe() override external payable {
+    function subscribe() override nonReentrant external payable {
+        _creatorFunds[commerceToken] += membershipPrice;
 
+        if (commerceToken == address(0)) {
+            require(msg.value == membershipPrice, "Price not matched");
+        } else {
+            _subscribeWithERC20();
+        }
+    }
+
+    /** 
+    @notice Purchase membership with an ERC-20 token
+    */
+    function _subscribeWithERC20() private {
+        require(msg.value == 0, "Ether is not accepted");
+        require(IERC20(commerceToken).allowance(msg.sender, address(this)) >= membershipPrice, "Token allowance is not sufficient");
+        IERC20(commerceToken).safeTransferFrom(msg.sender, address(this), membershipPrice);
     }
 
     /** 
@@ -64,6 +84,11 @@ IMembership {
     function claimRewards() override external {
 
     }
+
+    /**
+    @notice Receive ether arbitrarily
+     */
+    receive() external payable {} // TODO: Remove when the proper ETH acceptance mechanism is implemented
 
     /* ERC-721 FUNCTIONS */
 
