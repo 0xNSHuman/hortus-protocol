ccd1869770ac4fd60e8fdbb5e06829064a24259e
0xNSHuman <0xnshuman@protonmail.com>
2022-03-10 23:49:25 -0700

    Cover community deployment with tests

--- test/CommonityFactory.test.ts
+++ test/CommonityFactory.test.ts
@@ -18,6 +18,9 @@ describe('Community Factory', () => {
   let owner: SignerWithAddress;
   let creator: SignerWithAddress;
   let communityFactory: CommunityFactory;
+  let treasury: SignerWithAddress;
+
+  let feePolicy: any;
 
   const SYMBOL = "TST";
   const NAME = "Test Community";
@@ -25,66 +28,87 @@ describe('Community Factory', () => {
   const MEMBERSHIP_PRICE = ethers.utils.parseEther("0.05");
   
   beforeEach(async () => {
-    [owner, creator] = await ethers.getSigners();
+    [owner, creator, treasury] = await ethers.getSigners();
+
+    feePolicy = {
+      feeNumerator: 1,
+      feeDenomerator: 100,
+      feeReceiver: treasury.address,
+      feeSetter: owner.address
+    };
+
+    communityFactory = await new CommunityFactory__factory(owner).deploy(
+      feePolicy.feeNumerator,
+      feePolicy.feeDenomerator,
+      feePolicy.feeReceiver,
+      feePolicy.feeSetter
+    );
   });
 
   describe("constructor", () => {
     it("results in valid deployment",async () => {
-      communityFactory = await new CommunityFactory__factory(owner).deploy();
       expect(communityFactory.address).to.not.equal(null);
+    });
+
+    it("sets the expected fee policy",async () => {
+      fail("not implemented");
     });
   });
 
-  describe("community creation", () => {
-    beforeEach(async () => {
-      communityFactory = await new CommunityFactory__factory(owner).deploy();
+  describe('createCommunity', () => { 
+    it("allows whitelisted currencies only",async () => {
+      fail("not implemented");
+    });
+    
+    it("creates CommunityVault with matching parameters",async () => {
+      const tx = await communityFactory
+        .connect(creator)
+        .createCommunity(
+          SYMBOL,
+          NAME,
+          CURRENCY_TOKEN_ADDRESS,
+          MEMBERSHIP_PRICE
+        )
+
+      const receipt = await tx.wait();
+      const event = receipt.events?.find(e => e.event === "CommunityDeployment");
+      const [_, communityAddr] = event!.args!;
+
+      const community = (new CommunityVault__factory(owner).attach(communityAddr as string));
+
+      expect(await community.symbol()).to.equal(SYMBOL);
+      expect(await community.name()).to.equal(NAME);
+      expect(await community.commerceToken()).to.equal(CURRENCY_TOKEN_ADDRESS);
+      expect(await community.membershipPrice()).to.equal(MEMBERSHIP_PRICE);
     });
 
-    describe('createCommunity', () => { 
-      it("allows whitelisted currencies only",async () => {
-        fail("not implemented");
-      });
-      
-      it("creates CommunityVault with matching parameters",async () => {
-        const tx = await communityFactory
-          .connect(creator)
-          .createCommunity(
-            SYMBOL,
-            NAME,
-            CURRENCY_TOKEN_ADDRESS,
-            MEMBERSHIP_PRICE
-          )
+    it("emits the CommunityDeployment event with expected arguments",async () => {
+      const tx = await communityFactory
+        .connect(creator)
+        .createCommunity(
+          SYMBOL,
+          NAME,
+          CURRENCY_TOKEN_ADDRESS,
+          MEMBERSHIP_PRICE
+        )
 
-        const receipt = await tx.wait();
-        const event = receipt.events?.find(e => e.event === "CommunityDeployment");
-        const [_, communityAddr] = event!.args!;
+      const receipt = await tx.wait();
+      const event = receipt.events?.find(e => e.event === "CommunityDeployment");
+      const [_, communityAddr] = event!.args!;
 
-        const community = (new CommunityVault__factory(owner).attach(communityAddr as string));
+      expect(tx)
+        .to.emit(communityFactory, "CommunityDeployment")
+        .withArgs(creator.address, communityAddr as string);
+    });
+  });
 
-        expect(await community.symbol()).to.equal(SYMBOL);
-        expect(await community.name()).to.equal(NAME);
-        expect(await community.commerceToken()).to.equal(CURRENCY_TOKEN_ADDRESS);
-        expect(await community.membershipPrice()).to.equal(MEMBERSHIP_PRICE);
-      });
+  describe('setProtocolFeePolicy', () => { 
+    it("sets the expected fee policy",async () => {
+      fail("not implemented");
+    });
 
-      it("emits the CommunityDeployment event with expected arguments",async () => {
-        const tx = await communityFactory
-          .connect(creator)
-          .createCommunity(
-            SYMBOL,
-            NAME,
-            CURRENCY_TOKEN_ADDRESS,
-            MEMBERSHIP_PRICE
-          )
-
-        const receipt = await tx.wait();
-        const event = receipt.events?.find(e => e.event === "CommunityDeployment");
-        const [_, communityAddr] = event!.args!;
-
-        expect(tx)
-          .to.emit(communityFactory, "CommunityDeployment")
-          .withArgs(creator.address, communityAddr as string);
-      });
-     });
+    it("emits the ProtocolFeePolicyUpdate event with expected arguments",async () => {
+      fail("not implemented");
+    });
   });
  });
